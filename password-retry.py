af352d77a8da35b07da096199e336e8e4ff79ba7
Linhan Dang <17868741386@163.com>
2025-11-16 14:39:19 +0800

    change to while i > 0

--- password-retry.py
+++ password-retry.py
@@ -1,15 +1,16 @@
-password = 'a123456'	# 密码
-i = 3	# 剩余机会
 
-while True:		# 重复输入
+password = 'a123456'
+i = 3
+
+while i > 0:
 	pw = input('请输入密码：')
-	if pw == password:		# 如果 就
+	i = i - 1
+	if pw == password:
 		print('登入成功！')
 		break
-	else:		# 否则
-		i = i - 1
-		print('密码错误！还有', i, '次机会')
-		if i == 0:		# 如果 就
-			break
-
-
+	else:
+		print('密码错误！')
+		if i > 0:
+			print('还有', i, '次机会')
+		else:
+			print('账号将被锁定')
